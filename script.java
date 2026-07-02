c196f0d0718ae2ce6587ca9b68e89f9537d1b60c
sys-nerd <syscenaknaly@gmail.com>
2026-07-02 13:29:14 +0300

    Add files via upload

---
+++ script.java
@@ -0,0 +1,7 @@
+[15:57, 01/07/2026] cena: function welcome(){
+    alert("Welcome to Cena's Portfolio Website!");
+}
+
+console.log("Portfolio website loaded successfully.");
+[15:57, 01/07/2026] cena: function welcome(){
+    alert("Welcome to Cena's Portfolio Website!");
